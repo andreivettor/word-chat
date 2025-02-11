32e277aa8087b9ce2c7a7e41731cf7a61ecfbbd0
AndreiV <32334900+andreivettor@users.noreply.github.com>
2025-02-11 09:38:18 +0200

    Change frontend connection URL

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,6 +2,8 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:8080/";
+  process.env.NODE_ENV === "production"
+    ? "https://word-chat-server.onrender.com"
+    : "https://localhost:8080/";
 
 export const socket = io(URL);
